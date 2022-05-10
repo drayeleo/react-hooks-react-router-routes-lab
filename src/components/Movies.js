5b15ceb830f264fb9c28711cded0841494935404
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-05-09 22:26:18 -0600

    commit tree 6598f07686cfffe1384caedb13ebcbbab22fe50d

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,29 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  // console.log("movies here");
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map((movie) => {
+        return (
+          <div key={movie.title}>
+            <h1>Name: {movie.title}</h1>
+            <p>Time: {movie.time}</p>
+            <>
+              Genres:
+              <ul>
+                {movie.genres.map((genre, index) => (
+                  <li key={index}>{genre}</li>
+                ))}
+              </ul>
+            </>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Movies;
